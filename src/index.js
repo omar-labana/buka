419777d6adfe8d9ffaf4a570cefcf4fac04e2fe0
Omar Labana <omarlabana@outlook.com>
2021-08-04 15:25:56 +0200

    Edit project initlization

--- src/index.js
+++ src/index.js
@@ -1,1 +1,100 @@
-console.log(1)
+import Project from './Project'
+import Todo from './Todo'
+import { renderProject, fetchTodoForm, createTodo, setProjectsToStorage, getProjectsFromStorage } from './helpers'
+
+let projects = []
+
+if (localStorage.projects) {
+    projects = getProjectsFromStorage()
+} else {
+    console.log(localStorage.projects);
+    const defaultProject = new Project('default project')
+    const defaultTodoOne = new Todo('Cut chicken', 'do something', '2021-08-02', 'High')
+    const defaultTodoTwo = new Todo('boil water', 'do something', '2021-08-03', 'High')
+
+    defaultProject.todos.push(defaultTodoOne)
+    defaultProject.todos.push(defaultTodoTwo)
+
+
+    projects.push(defaultProject)
+    console.log(projects);
+    setProjectsToStorage(projects)
+}
+
+
+const contentTag = document.getElementById('content')
+
+const renderProjects = (projects) => {
+    contentTag.innerHTML = ''
+    projects.forEach(project => {
+        contentTag.appendChild(renderProject(project))
+    })
+};
+
+renderProjects(projects)
+
+// Initial load
+getProjectsFromStorage()
+
+// add project
+const addProjectBtn = document.getElementById('project-modal-btn');
+const projectModal = document.getElementById('project-modal');
+const closeProjectModal = document.getElementById('project-close-icon');
+
+addProjectBtn.onclick = () => {
+    projectModal.style.display = 'block';
+};
+closeProjectModal.onclick = () => {
+    projectModal.style.display = 'none';
+};
+window.onclick = (event) => {
+    if (event.target === projectModal) {
+        projectModal.style.display = 'none';
+    }
+};
+const addProject = (e) => {
+    e.preventDefault();
+    const projectName = document.getElementById('project-name').value;
+    const newProject = new Project(projectName)
+    projects.push(newProject)
+    setProjectsToStorage(projects)
+    contentTag.appendChild(renderProject(newProject))
+    projectModal.style.display = 'none';
+
+}
+const projectForm = document.getElementById('add-project');
+projectForm.addEventListener('submit', addProject);
+// add todo
+
+const todoModal = document.getElementById('todo-modal');
+const closeTodoModal = document.getElementById('todo-close-icon');
+
+closeTodoModal.onclick = () => {
+    todoModal.style.display = 'none';
+};
+window.onclick = (event) => {
+    if (event.target === todoModal) {
+        todoModal.style.display = 'none';
+    }
+};
+const addTodo = (e) => {
+    e.preventDefault();
+    const data = fetchTodoForm()
+
+    const projectIndex = projects.findIndex(project => project.name === localStorage.target)
+
+    const project = projects[projectIndex]
+    const todo = new Todo(data.title, data.description, data.dueDate, data.priority)
+    project.todos.push(todo)
+
+    document.getElementById(project.name.replace(/\s/g, '-')).appendChild(
+        createTodo(todo)
+    )
+    setProjectsToStorage(projects)
+    renderProjects(projects)
+}
+const todoForm = document.getElementById('add-todo');
+todoForm.addEventListener('submit', addTodo);
+
+
+//test
