0b946da2288c282d5d478b876fd898b41d3652d3
Omar Labana <omarlabana@outlook.com>
2021-08-08 13:42:57 +0200

    Fix linter errors

--- src/test/project.test.js
+++ src/test/project.test.js
@@ -1,27 +1,26 @@
-import Project from '../Project'
+import Project from '../Project';
 
 describe('Project instance members', () => {
-    const project = new Project('Project');
+  const project = new Project('Project');
 
-    const dummyProject = {
-        name: 'Project',
-        todos: []
-    }
+  const dummyProject = {
+    name: 'Project',
+    todos: [],
+  };
 
-    it('checks the type of created instance', () => {
-        expect(typeof project).toBe('object')
-    });
+  it('checks the type of created instance', () => {
+    expect(typeof project).toBe('object');
+  });
 
-    it('checks object structure', () => {
-        expect(project).toMatchObject(dummyProject);
-    });
+  it('checks object structure', () => {
+    expect(project).toMatchObject(dummyProject);
+  });
 
-    it('checks default value if todos array', () => {
-        expect(project.todos).toStrictEqual([])
-    });
+  it('checks default value if todos array', () => {
+    expect(project.todos).toStrictEqual([]);
+  });
 
-    it('checks number of instance properties', () => {
-        expect(Object.keys(project).length
-        ).toBe(Object.keys(dummyProject).length)
-    });
+  it('checks number of instance properties', () => {
+    expect(Object.keys(project).length).toBe(Object.keys(dummyProject).length);
+  });
 });
